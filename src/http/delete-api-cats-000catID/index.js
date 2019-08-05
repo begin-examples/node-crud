438cc8d5c0e93c09aa9249dc5a51467ad70046f9
kj <kristoferjoseph@users.noreply.github.com>
2019-08-05 13:10:17 -0700

    _ci: removing src/http/delete-api-cats-000catID/index.js for route

--- src/http/delete-api-cats-000catID/index.js
+++
@@ -1,9 +0,0 @@
-let data = require('@begin/data')
-
-exports.handler = async function destroy(req) {
-  await data.destroy({
-    table: 'cats',
-    key: req.params.catID
-  })
-  return {statusCode: 201}
-}
