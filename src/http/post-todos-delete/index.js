b82fa66439978c23297ad7fff4b234585993ac57
kj <kristoferjoseph@gmail.com>
2019-08-07 18:10:31 -0700

    First pass of fully working REST version.

--- src/http/post-todos-delete/index.js
+++ src/http/post-todos-delete/index.js
@@ -1,17 +1,12 @@
 let data = require('@begin/data')
 
 exports.handler = async function destroy (req) {
-  let id = req.body.id
-  console.log(`DESTROY ${id}`)
   await data.destroy({
     table: 'todos',
-    key: id
+    ...req.body
   })
   return {
-    statusCode: 201,
-    type: 'application/json; charset=utf8',
-    body: JSON.stringify({
-      ok: true
-    })
+    statusCode: 302,
+    location: '/'
   }
 }
