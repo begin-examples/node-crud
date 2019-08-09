3fb2852b7a36c59d7e5a790986cb70fd8526418d
kj <kristoferjoseph@gmail.com>
2019-08-09 16:20:38 -0700

    Adds save and delete symbols.

--- src/http/get-todos/index.js
+++ src/http/get-todos/index.js
@@ -4,10 +4,8 @@ exports.handler = async function todos () {
   let todos = await data.get({
     table: 'todos'
   })
-
   // Return oldest todo first
   todos.sort((a, b) => a.created > b.created)
-
   return {
     status: 201,
     type: 'application/json; charset=utf8',
--- src/http/post-todos/index.js
+++ src/http/post-todos/index.js
@@ -7,30 +7,8 @@ exports.handler = async function post (req) {
     table: 'todos',
     ...todo
   })
-  if (isXHR(req.headers)) {
-    return {
-      statusCode: 201
-    }
-  } else {
-    return {
-      statusCode: 302,
-      location: '/'
-    }
+  return {
+    statusCode: 302,
+    location: '/'
   }
-}
-
-function isXHR (headers) {
-  if (!headers) {
-    return false
-  }
-
-  if (headers['X-Requested-With']) {
-    return headers['X-Requested-With'] === 'XMLHttpRequest'
-  }
-
-  if (headers['x-requested-with']) {
-    return headers['x-requested-with'] === 'XMLHttpRequest'
-  }
-
-  return false
 }
