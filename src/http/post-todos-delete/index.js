b70c04afaa3a652d9b106a4cecbb355aa8958264
Ryan Block <rblock@gmail.com>
2019-09-16 08:48:46 -0700

    Update deps and deletion logic

--- src/http/post-todos-delete/index.js
+++ src/http/post-todos-delete/index.js
@@ -1,9 +1,11 @@
+let arc = require('@architect/functions')
 let data = require('@begin/data')
 
 exports.handler = async function destroy (req) {
+  let key = arc.http.helpers.bodyParser(req).key // Base64 decodes + parses body
   await data.destroy({
     table: 'todos',
-    ...req.body
+    key
   })
   return {
     statusCode: 302,
