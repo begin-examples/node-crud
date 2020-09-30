57620598f91ec00d1fa76922db8abb1458d87208
kj <kristoferjoseph@gmail.com>
2020-09-30 16:04:29 -0700

    Adds pagination.

--- src/http/get-todos/index.js
+++ src/http/get-todos/index.js
@@ -1,11 +1,18 @@
 const data = require('@begin/data')
 
 exports.handler = async function todos (req) {
-  let todos = await data.get({
-    table: 'todos'
+  let pages = await data.get({
+    table: 'todos',
+    limit: 25
   })
+
+  let todos = []
+  for await (let todo of pages) {
+    todos.push(todo)
+  }
   // Return oldest todo first
   todos.sort((a, b) => a.created > b.created)
+
   return {
     statusCode: 201,
     headers: {
